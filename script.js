b8ac2909acd105cb608f5c08759cb42fa5ba0e7e
jfark <jessamyn.farkas@gmail.com>
2019-02-17 22:23:31 -0500

    debugged speechMath

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ var mySpeeches = [
 		"author" : "Winston Churchill",
 		"date" : 1940,
 		"b.c.e." : false
-	};
+	},
 	{
 		"author" : "Ghandi",
 		"date" : 1942,
 		"b.c.e." : false
-	}; 
+	}, 
 	{
 		"author" : "Demosthenes",
 		"date" : 342,
 		"b.c.e." : true
-	};
-]
+	}
+];
 
-var speechMath = (mySpeeches[1].year - mySpeeches[0].year);
+var speechMath = [mySpeeches[1].date - mySpeeches[0].date];
 
-console.log onload("Ghandi's speech and Churchill's speech are" + speechMath + " years apart.")
+console.log("Ghandi's speech and Churchill's speech are " + speechMath + " years apart.");
 
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
